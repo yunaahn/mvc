c0452b96dc56d9960a6efbbba620c171767d7c77
yunaahn <nayou0108@gmail.com>
2024-04-03 14:32:38 +0900

    login

---
+++ login/src/main/java/hello/login/web/SessionConst.java
@@ -0,0 +1,10 @@
+package hello.login.web;
+
+//상수만 갖다쓰는것이라 인터페이스로 구현
+//public interface SessionConst {
+//    String LOGIN_MEMBER = "loginMember";
+//}
+
+public class SessionConst {
+    public  static final String  LOGIN_MEMBER = "loginMember";
+}
